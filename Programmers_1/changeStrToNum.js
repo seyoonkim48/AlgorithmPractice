25ca4f66cfb1a0c450fd13d921b1bc3c9dcf3f96
seyoonkim48 <seyoonkim48@gmail.com>
2021-07-30 23:32:52 +0900

    문자열을 정수로 바꾸기

---
+++ Programmers_1/changeStrToNum.js
@@ -0,0 +1,9 @@
+function solution(s) {
+    // if(s[0] === '-') {
+    //     s = -1 * Number(s.slice(1));
+    // } else {
+    //     s = Number(s);
+    // }
+    // return s;
+    return Number(s);
+}
