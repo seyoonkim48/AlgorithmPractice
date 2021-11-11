ebe9f0248383b6b305b715c5b97445d6bddbef66
seyoonkim48 <seyoonkim48@gmail.com>
2021-11-11 23:59:10 +0900

    기능개발

---
+++ Programmers_2/upgradeProgresses.js
@@ -0,0 +1,8 @@
+function solution(progresses, speeds) {
+    const workPerDays = progresses.map((el, i) => Math.ceil((100 - el) / speeds[i]));
+    let result = [];
+    let now = workPerDays;
+    while(workPerDays.length) {
+        if(now < )
+    }
+}
