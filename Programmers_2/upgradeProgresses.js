224d83df67369bfc3157841baf954de62c673b79
seyoonkim48 <seyoonkim48@gmail.com>
2021-11-12 22:29:02 +0900

    기능개발_최종

--- Programmers_2/upgradeProgresses.js
+++ Programmers_2/upgradeProgresses.js
@@ -1,8 +1,24 @@
 function solution(progresses, speeds) {
+    // (100(완성) - 현재 진행) / 속도 => 완성까지 걸리는 날
     const workPerDays = progresses.map((el, i) => Math.ceil((100 - el) / speeds[i]));
     let result = [];
-    let now = workPerDays;
+    // 첫번째 일을 now에 담는다.
+    let now = workPerDays[0];
+    let count = 1;
     while(workPerDays.length) {
-        if(now < )
+        // 맨 앞부터 하나씩 빼가며 비교한다.
+        workPerDays.shift();
+        if (workPerDays.length === 0) {
+            result.push(count);
+            break;
+        }
+        if (now < workPerDays[0]) {
+            result.push(count);
+            count = 1;
+            now = workPerDays[0];
+        } else {
+            count++;
+        }
     }
+    return result;
 }
