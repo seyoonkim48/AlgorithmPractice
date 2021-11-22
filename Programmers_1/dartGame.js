381a56999e2c501fc0b0428a1bb0096bed29c3a6
seyoonkim48 <83910753+seyoonkim48@users.noreply.github.com>
2021-11-22 23:58:25 +0900

    다트게임

--- Programmers_1/dartGame.js
+++ Programmers_1/dartGame.js
@@ -1,5 +1,6 @@
 function solution(dartResult) {
-    // 총 세 번의 기회
-    // 각 기회마다 0~10
-    // S, D( ^2), T( ^3)
+    let result, temp;
+    const arr = [];
+    
+    return arr.reduce((acc, cur) => acc + cur, 0);
 }
